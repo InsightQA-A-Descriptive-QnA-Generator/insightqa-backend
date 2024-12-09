632475a29c0d84069aabacd14710ecee894b15ca
azeem30 <pinjariashpak3@gmail.com>
2024-12-09 15:56:27 +0530

    Port Changed

--- app.py
+++ app.py
@@ -16,4 +16,5 @@ def generate():
     return jsonify({"questions_and_answers": result})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
+    app.run(debug=True, host='0.0.0.0', port=port)
